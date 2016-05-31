a1e22827c9f4fe1efb17864877d31ae1641bc1f7
892280082 <892280082@qq.com>
2016-05-31 11:07:33 +0800

    OK

--- src/ArrayPagin.js
+++ src/ArrayPagin.js
@@ -6,9 +6,9 @@
  * @API
  * ------------------------------------------------
  * 1.__init				初始化
- * 2.$setCurPage		跳转到制定页
- * 3.$save				添加一个对象
- * 4.$remove			删除一个对象				
+ * 2.setCurPage		跳转到制定页
+ * 3.save				添加一个对象
+ * 4.remove			删除一个对象				
  * ------------------------------------------------
  */
 
@@ -25,7 +25,7 @@ var util = require('./util');
 var ArrayPagin;
 module.exports = ArrayPagin = function(){
 	//分页信息
-	this.$pagingInfo = {
+	this.pagingInfo = {
 	    total:0,
 	    curPage:1,
 	    pageSize:20,
@@ -34,14 +34,14 @@ module.exports = ArrayPagin = function(){
 	};
 
 	//显示对象
-	this.$array = [];
+	this.array = [];
 
 	//缓存
 	this._array = [];
 };
 
 ArrayPagin.prototype.__init = function(array,pageSize,waterfull){
-	var pagingInfo = this.$pagingInfo;
+	var pagingInfo = this.pagingInfo;
 
 	this._array = array || this._array;
 
@@ -53,20 +53,20 @@ ArrayPagin.prototype.__init = function(array,pageSize,waterfull){
 
 	pagingInfo.allPage = util.juageAllPage(pagingInfo.pageSize,pagingInfo.total);
 
-	return this.$setCurPage();
+	return this.setCurPage();
 };
 
-ArrayPagin.prototype.$setCurPage = function(curPage){
-		var pagingInfo = this.$pagingInfo;
+ArrayPagin.prototype.setCurPage = function(curPage){
+		var pagingInfo = this.pagingInfo;
 		if(curPage)
 			pagingInfo.curPage = curPage;
 
-		this.$array = util.getSlice(this._array,pagingInfo.curPage,pagingInfo.pageSize);
+		this.array = util.getSlice(this._array,pagingInfo.curPage,pagingInfo.pageSize);
 		return this;
 };
 
 /**@param {Object|Array} pojo 添加数组或者对象  */
-ArrayPagin.prototype.$save = function(pojo){
+ArrayPagin.prototype.save = function(pojo){
 	pojo instanceof Array ? this._array = this._array.concat(pojo) 
 						  : this._array.push(pojo);
 
@@ -74,7 +74,7 @@ ArrayPagin.prototype.$save = function(pojo){
 	return this;	
 };
 
-ArrayPagin.prototype.$remove = function(pojo){
+ArrayPagin.prototype.remove = function(pojo){
 	util.arrayRemovePojo(this._array,pojo);
 	this.$init();
 	return this;
